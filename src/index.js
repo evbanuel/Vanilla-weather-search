fd8209af3c7492cfe39f666b79bb3886bcc916d8
Evelyn Banuelos <eobo0803@gmail.com>
2026-06-03 15:39:25 -0700

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+     let temperature = response.data.temperatur.currennt;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city){
+let apiKey = "d03tbf689f00a95a61d59c0o4a2ad7d9";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
 
+searchCity(searchInput.value);
 }
 
 
