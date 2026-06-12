2243db9f0ca6f66043d82cae1d0284c1c40f5c63
Evelyn Banuelos <eobo0803@gmail.com>
2026-06-12 10:21:24 -0700

    Inject the forecast HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -54,6 +54,36 @@ function handleSearchSubmit(event){
 searchCity(searchInput.value);
 }
 
+function displayForecast(){
+    let days = ["Tues","Wed","Thu","Fri","Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function(day){
+forecastHtml =
+forecastHtml +
+
+` <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌥️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>
+`;
+    });
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+
+}
+
+
+
+
+
+
 
 
 
@@ -63,3 +93,4 @@ let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
